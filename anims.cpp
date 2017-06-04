8ade1541a846c172c83b080cc569277846260ed1
Pedro <pedroignacioperezvargas@gmail.com>
2017-06-04 02:31:42 -0300

    Done lots of things

--- anims.cpp
+++ anims.cpp
@@ -1,23 +1,36 @@
+#ifndef ANIMS
 #include "anims.hpp"
+#endif // !ANIMS
 
-void KeyFrames::set( int dir, int type, float args[2] ) {
+void AnimsSFML::KeyFrames::set( int dir, int type, float args[2] ) {
+	if( dir >= totalFrames || dir < 0 ) {
+		throw std::invalid_argument( "frame* KeyFrames::get( int dir )\n'dir' has to be between 0 and " + totalFrames );
+	}
+	#ifdef _DEBUG
 	std::cout << "Setting frame " << dir << std::endl;
+	#endif // _DEBUG
 	frames[dir].args = args;
 	frames[dir].type = type;
 }
-void KeyFrames::set( int dir, int type, float arg ) {
+void AnimsSFML::KeyFrames::set( int dir, int type, float arg ) {
+	if( dir >= totalFrames || dir < 0 ) {
+		throw std::invalid_argument( "frame* KeyFrames::get( int dir )\n'dir' has to be between 0 and " + totalFrames );
+	}
+	#ifdef _DEBUG
 	std::cout << "Setting frame " << dir << std::endl;
+	#endif // _DEBUG
 	frames[dir].arg = arg;
 	frames[dir].type = type;
 }
-frame* KeyFrames::get( int dir ) {
-	if( dir < totalFrames && dir >= 0 ) {
-		return &frames[dir];
+AnimsSFML::frame* AnimsSFML::KeyFrames::get( int dir ) {
+	if( dir >= totalFrames || dir < 0 ) {
+		throw std::invalid_argument("frame* KeyFrames::get( int dir )\n'dir' has to be between 0 and "+totalFrames);
 	}
+	return &frames[dir];
 }
 
 template <typename T>
-void Animation<T>::Move( sf::Vector2<int> toPos, bool absolute, int from = 0, int to = 27 ) {
+void AnimsSFML::Animation<T>::Move( sf::Vector2<int> toPos, bool absolute, unsigned int from = 0, unsigned int to = 27 ) {
 
 	iniPos = { ( *object ).getPosition().x, ( *object ).getPosition().y };
 	int framesRange = to - from;
@@ -41,3 +54,49 @@ void Animation<T>::Move( sf::Vector2<int> toPos, bool absolute, int from = 0, in
 	}
 	cout << steps.x;
 }
+template <typename T>
+void AnimsSFML::Animation<T>::Rotate( int angle, bool absolute, int from, int to ) {
+	//if( from > to || from >= frames || to >= frames ) return;//If arguments are invalid, exit
+	int framesRange = to - from;
+	iniRot = ( *object ).getRotation();
+	float step;
+	step = (float)angle / framesRange;
+	for( int i = from; i < to; i++ ) {
+		float frm = step;
+		if( absolute ) {
+			frm *= i;
+			frm += iniRot;
+			kFrames.set( i, ROT + ABS, frm );
+		} else {
+			kFrames.set( i, ROT, frm );
+		}
+	}
+}
+template <typename T>
+void AnimsSFML::Animation<T>::ProcessFrame( int frm ) {
+	frame* f = kFrames.get( frm );
+	#ifdef _DEBUG
+	/*std::cout << "\nProcessing frame " << frm << std::endl <<
+		" Type: " << ( *f ).type << std::endl;
+	if( ( *f ).type&POS )
+		std::cout << " Args: " << ( *f ).args[0] << "&" << ( *f ).args[1];
+	else
+		std::cout << " Arg: " << ( *f ).arg;
+		*/
+	#endif // _DEBUG
+	if( ( *f ).type&POS ) {
+		if( ( *f ).type&ABS ) {//Absolute duh...
+			( *object ).setPosition( ( *f ).args[0], ( *f ).args[1] );
+		} else {//Relative
+			( *object ).setPosition( iniPos.x + ( *f ).args[0], iniPos.y + ( *f ).args[1] );
+		}
+	} else if( ( *f ).type&ROT ) {
+		if( ( *f ).type&ABS ) {
+			( *object ).setRotation( ( *f ).arg );
+		} else {
+			( *object ).setRotation( ( *f ).arg + ( *object ).getRotation() );
+		}
+	}
+	actFrame = actFrame == ( frames - 1 )?0:actFrame + 1;
+}
+
--- anims.hpp
+++ anims.hpp
@@ -1,9 +1,10 @@
-#pragma once
-
 #ifndef ANIMS
 #define ANIMS
 
+#pragma once
+
 #include <string>
+#include <deque>
 
 #ifdef _DEBUG 
 #include <iostream>
@@ -11,148 +12,90 @@
 
 #include <SFML\Graphics.hpp>
 
-#define ROT 1//Rotacion
-#define POS 2//Posicion
-#define ABS 16//Modificador absoluto
+#define ROT 1//Rotation
+#define POS 2//Position
+#define ABS 16//Absolute modifier
 
-struct frame {
-	int type = 0;
-	//std::string args = "";
-	float* args = new float [2];//For POS
-	float arg = 0;//For ROT
-};
+namespace AnimsSFML {
+	struct frame {
+		int type = 0;
+		float* args = new float[2];//For POS
+		float arg = 0;//For ROT
+	};
 
-class KeyFrames {
+	class KeyFrames {
 	public:
 		frame * frames;
 		int totalFrames = 0;
 		//char ** r = new char*[2];
 		//Member initialization
-		KeyFrames(int frameAmount) {
+		KeyFrames() {}
+		void ini( int frameAmount ) {
 			totalFrames = frameAmount;
 			frames = new frame[totalFrames];
-		}
-		KeyFrames() {
-			totalFrames = 27;
-			frames = new frame[totalFrames];
+			#ifdef _DEBUG
+			std::cout << "Object initialized with " << totalFrames << " frames" << std::endl;
+			#endif
 		}
 		void set( int dir, int type, float args[2] );
-		void set( int dir, int type, float arg/*std::string args*/ );
+		void set( int dir, int type, float arg );
 		frame* get( int dir );
-		/*void set(int dir,char act,char arg) {
-			actions[dir] = act;
-			args[dir] = arg;
-		}
-		char** operator[](int dir) {
-			//char * r = new char[2];
-			r[0] = &actions[dir];
-			r[1] = &args[dir];
-			//std::cout << "[]";
-			return r;
-		}
-		char** operator=( const char** arr ) {
-			std::cout << arr;
-			return r;
-		}*/
-		/*KeyFrames& operator=( const KeyFrames& x ) {
-			r[0] = actions[dir];
-			r[1] = args[dir];
-			std::cout << "=";
-			return r;
-		}*/
-};
+	};
 
-template <typename T>
-class Animation {
+	template <typename T>
+	class Animation {
 		T object;
 		sf::Vector2<float> iniPos;
 		int iniRot;
+		static unsigned int initialized;//Objects initialized
 	public:
+		//static Animation<T> * Animations;
+		static std::deque<void*> Animations;
+		unsigned int ID;//ID of the new object
 		int actFrame = 0, frames = 1;
 		float frameRate = 27;
 		KeyFrames kFrames;
-		Animation(T x,int frameAmount) {
+		Animation( T x, int frameAmount ) {
 			object = x;
 			frames = frameAmount;
-			KeyFrames kFrames(frameAmount);
+			kFrames.ini( frameAmount );
 			iniPos = { ( *object ).getPosition().x, ( *object ).getPosition().y };
 			iniRot = ( *object ).getRotation();
-		}
-		void Animation::Move( sf::Vector2<int> toPos, bool absolute, int from = 0, int to = 27 );
-		/*void Animation::Rotate( int angle, bool absolute ) {//Shorthand
-			iniRot = ( *object ).getRotation();
-			float step;
-			step = (float)angle / frameRate;
-			for( int i = 0; i < frames; i++ ) {
-				float frm = step;
-				if( absolute ) {
-					frm *= i;
-					frm += iniRot;
-					kFrames.set( i, ROT + ABS, frm );
-				} else {
-					kFrames.set( i, ROT, frm );
-				}
+			ID = initialized;
+			//Animations[ID] = this;
+			if( Animations.size() <= ID ) {
+				Animations.push_front( this );
+			} else {
+				Animations.at( ID ) = this;
 			}
-			//this::Rotate( angle, absolute, 0, frames);
-		}*/
-		void Animation::Rotate( int angle, bool absolute, int from, int to ) {
-			//if( from > to || from >= frames || to >= frames ) return;//If arguments are invalid, exit
-
-			int framesRange = to - from;
-			iniRot = ( *object ).getRotation();
-			float step;
-			step = (float)angle / framesRange;
-			for( int i = from; i < to; i++ ) {
-				float frm = step;
-				if( absolute ) {
-					frm *= i;
-					frm += iniRot;
-					kFrames.set( i, ROT + ABS, frm );
-				} else {
-					kFrames.set( i, ROT, frm );
-				}
-			}
-		}
-		void Animation::ProcessFrame( int frm ) {
-			frame* f = kFrames.get( frm );
+			initialized++;
 			#ifdef _DEBUG
-			std::cout << "Processing frame " << frm << std::endl <<
-				" Type: " << ( *f ).type << std::endl;
-			if( ( *f ).type&POS ) 
-				std::cout << " Args: " << ( *f ).args[0] << "&" << ( *f ).args[1];
-			else
-				std::cout << " Arg: " << ( *f ).arg;
-
+			std::cout << "Initialized instance with ID " << ID << " at the addrs " << reinterpret_cast<int>( this ) << std::endl;
 			#endif // _DEBUG
-			if( ( *f ).type&POS ) {
-				if( ( *f ).type&ABS ) {//Absolute duh...
-					( *object ).setPosition( ( *f ).args[0], ( *f ).args[1] );
-				} else {//Relative
-					( *object ).setPosition( iniPos.x + ( *f ).args[0], iniPos.y + ( *f ).args[1] );
-				}
-			} else if( ( *f ).type&ROT ) {
-				if( ( *f ).type&ABS ) {
-					( *object ).setRotation( ( *f ).arg );
-				} else {
-					( *object ).setRotation( ( *f ).arg + ( *object ).getRotation() );
-				}
-			}
-			if( actFrame == frames - 1 ) {
-				//animationEnd();
-			}
-			actFrame = actFrame == ( frames - 1 )?0:actFrame + 1;
+
 		}
+		void Animation::Move( sf::Vector2<int> toPos, bool absolute, unsigned int from = 0, unsigned int to = 27 );
+		void Animation::Rotate( int angle, bool absolute, int from, int to );
+		void Animation::ProcessFrame( int frm );
 		#ifdef _DEBUG
 		void Animation::printDebug() {
-			//std::cout << kFrames.get( 0 )[1];
+			std::cout << "Animation instance with ID " << ID << " of " << initialized << std::endl;
 			for( int i = 0; i < frames; i++ ) {
-				std::cout << "Frame: " << i << " Type: " << ( *kFrames.get( i ) ).type << std::endl;
+				std::cout << "Frame: " << i << " Type: " << ( kFrames.get( i )[0] ).type << std::endl;
 			}
 			std::cout << "\nActual frame: " << actFrame << "/" << frames << " at " << frameRate << "fps\n";
 		}
 		#endif // _DEBUG
-		//void Animation::animationEnd();
-		//void Animation::Animate( int animType, )
-};
+	};
+
+	void frameControllerThread() {
+
+	}
+}
+
+
+template <typename T> unsigned int AnimsSFML::Animation<T>::initialized = 0;
+template <typename T> std::deque<void*> AnimsSFML::Animation<T>::Animations = std::deque<void*>(1);
+
 
 #endif // !ANIMS
